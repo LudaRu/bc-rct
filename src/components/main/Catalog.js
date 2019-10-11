e34e3ef8f85adafc40d7b0dd82eb0df3361e61f3
Ruslan M <manizin.r.s@effective-group.ru>
2019-10-11 17:02:15 +0300

    x

--- src/components/main/Catalog.js
+++ src/components/main/Catalog.js
@@ -8,14 +8,23 @@ import Image from "react-bootstrap/Image";
 import ResponsiveEmbed from "react-bootstrap/ResponsiveEmbed";
 import Item from "./catalog/Item";
 
+const item = {
+    id: 1,
+    imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Square_Lattice_Tiling.svg/220px-Square_Lattice_Tiling.svg.png',
+    title: 'Автоклав бойлер формовочный компрессор',
+    tags: ['123 '],
+    createTime: '12:44',
+    isLike: true,
+};
+
 class Catalog extends Component {
     render() {
-        const {match} = this.props
+        const {match} = this.props;
         return (
             <Container fluid>
                 <Row noGutters>
                     {[...Array(50).keys()].map(v => {
-                        return <Col className={``} xs={12} sm={6} lg={3}><div className={`p-1`}><Item/></div></Col>
+                        return <Col className={``} xs={12} sm={6} lg={3}><div className={`p-1`}><Item item={item}/></div></Col>
                     })}
                 </Row>
             </Container>
--- src/components/main/catalog/Item.js
+++ src/components/main/catalog/Item.js
@@ -7,25 +7,19 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 class Item extends Component {
     render() {
-        const { match } = this.props
+        const { item } = this.props;
         return (
             <div className={`bg-light item`}>
                 <div className={`item-img`}>
-                    <Image
-                        fluid
-                        rounded
-                        src={`https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Square_Lattice_Tiling.svg/220px-Square_Lattice_Tiling.svg.png`}
-                    />
+                    <Image fluid rounded src={item.imgUrl} />
                 </div>
                 <div className={`item-content p-2`}>
                     <div className={`content`}>
-                        <div className={"title"}>Ааааааааааааааааааааааааааааааааааааааdddddddddddddddddddddаа</div>
-                        <small className="text-primary">@sergey</small>
+                        <div className={"title"}>{item.title}</div>
+                        <small className="text-primary">{item.tags   }</small>
                         <div className="d-flex justify-content-between s3 footer">
-                            <small>Добавлен: 17:25</small>
-                            <small>
-                                <FontAwesomeIcon icon={faHeart} />
-                            </small>
+                            <small>Добавлен: {item.createTime}</small>
+                            <FontAwesomeIcon className={'' + (item.isLike ? 'text-danger' : '')} icon={faHeart} />
                         </div>
                     </div>
                 </div>
