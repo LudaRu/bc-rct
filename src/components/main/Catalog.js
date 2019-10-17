28246cea7e0d74986bff3c3a7c765c5498c6880b
Ruslan M <manizin.r.s@effective-group.ru>
2019-10-17 18:14:31 +0300

    x

--- src/components/main/Catalog.js
+++ src/components/main/Catalog.js
@@ -7,6 +7,8 @@ import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
 import ResponsiveEmbed from "react-bootstrap/ResponsiveEmbed";
 import Item from "./catalog/Item";
+import Masonry from 'react-masonry-css'
+import posed, { PoseGroup } from 'react-pose'
 
 const item = {
     id: 1,
@@ -17,18 +19,47 @@ const item = {
     isLike: false,
 };
 
+// xs	<576px
+// true | "auto" | number | { span: true | "auto" | number, offset: number, order: number }
+//
+// sm	≥576px
+// true | "auto" | number | { span: true | "auto" | number, offset: number, order: number }
+//
+// md	≥768px
+// true | "auto" | number | { span: true | "auto" | number, offset: number, order: number }
+//
+// lg	≥992px
+// true | "auto" | number | { span: true | "auto" | number, offset: number, order: number }
+//
+// xl	≥1200px
+// true | "auto" | number | { span: true | "auto" | number, offset: number, order: number }
+
+const Itemx = posed.div({
+    enter: { opacity: 1 },
+    exit: { opacity: 0 }
+})
+
 class Catalog extends Component {
     render() {
         const {match} = this.props;
         return (
             <Container>
+                <Masonry
+                    breakpointCols={{default: 3,992: 2,768: 1,}}
+                    className="my-masonry-grid"
+                    columnClassName="my-masonry-grid_column">
+                    {[...Array(50).keys()].map(v => {
+                        return <Item item={item}/>
+                    })}
+                </Masonry>
+
                 <Row>
                     {[...Array(50).keys()].map(v => {
-                        return <Col className={``} xs={12} md={6} lg={4}>
-                            <div className={`pb-3 pt-sm-3 pb-sm-3`}>
-                                <Item item={item}/>
-                            </div>
-                        </Col>
+                        // return <Col className="" xs={12} md={6} lg={4}>
+                        //     <div className={`pb-3 pt-sm-3 pb-sm-3`}>
+                        //         <Item item={item}/>
+                        //     </div>
+                        // </Col>
                     })}
                 </Row>
             </Container>
--- src/components/main/catalog/Item.js
+++ src/components/main/catalog/Item.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Image} from "react-bootstrap";
+import {Col, Form, Image, InputGroup} from "react-bootstrap";
 // import { AnimateOnChange } from 'react-animation'
 import posed, {PoseGroup} from 'react-pose';
 
@@ -8,6 +8,8 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEdit, faHeart as farHeart, faTimesCircle} from '@fortawesome/free-regular-svg-icons';
 import {faHeart, faCheckCircle, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
 import {easing} from "popmotion";
+import Input from "../../../form/Input";
+import FormExample from "../../test/FormExample";
 
 
 // const Box = posed.div({
@@ -37,132 +39,165 @@ import {easing} from "popmotion";
 // });
 
 const Box = posed.div({
-	pressable: true,
-	init: {scale: 1},
-	press: {scale: 1.3},
-	pressEnd: {
-		scale: 1,
-		transition: {
-			type: 'spring',
-			stiffness: 500
-		}
-	},
+    pressable: true,
+    init: {scale: 1},
+    press: {scale: 1.2},
+    pressEnd: {
+        scale: 1,
+        transition: {
+            type: 'spring',
+            stiffness: 500
+        }
+    },
+});
+
+const Box2 = posed.div({
+    edit: {
+        applyAtEnd: { display: 'none' },
+        opacity: 0,
+        height: 0,
+        width: 0,
+        transition: {
+            type: 'tween',
+            duration: 250,
+        }
+
+    },
+    view: {
+        applyAtStart: { display: 'flex' },
+        opacity: 1,
+        height: "auto",
+        width: "auto",
+        transition: {
+            type: 'tween',
+            duration: 300,
+        }
+
+    },
+});
+
+const Editorn = posed.div({
+    edit: {
+        applyAtStart: { display: 'flex' },
+        opacity: 1,
+        height: "auto",
+        transition: {
+            type: 'tween',
+            duration: 300,
+            ease: "circOut",
+        }
+
+    },
+    view: {
+        opacity: 0,
+        height: 0,
+        applyAtEnd: { display: 'none'},
+        transition: {
+            type: 'tween',
+            duration: 250,
+            ease: "circOut",
+        }
+    },
 });
 
 const BoxItem = posed.div({
-	blob: {
-		zIdex: 100,
-		scale: 1.1,
-		transition: {
-			type: 'spring',
-			stiffness: 500
-		}
-	},
-	none: {
-		zIdex: 100,
-		scale: 1,
-		transition: {
-			type: 'spring',
-			stiffness: 500
-		}
-	},
+    blob: {},
+    none: {},
 });
 
 class Item extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isLike: props.item.isLike,
-			mode: 'view',
-			isBlob: false,
-		};
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLike: props.item.isLike,
+            mode: 'view',
+            isBlob: false,
+        };
 
-		this.toggleLike = this.toggleLike.bind(this);
-		this.toggleMode = this.toggleMode.bind(this);
-		this.renderImage = this.renderImage.bind(this);
-		this.renderTitle = this.renderTitle.bind(this);
-		this.renderTags = this.renderTags.bind(this);
+        this.toggleLike = this.toggleLike.bind(this);
+        this.toggleMode = this.toggleMode.bind(this);
+        this.toggleBlob = this.toggleBlob.bind(this);
+        this.renderToolbar = this.renderToolbar.bind(this);
+    }
+
+    toggleBlob() {
+        this.setState({isBlob: !this.state.isBlob});
+    }
+
+    toggleLike() {
+        this.setState({isLike: !this.state.isLike});
+    }
+
+    toggleMode() {
+        this.setState({mode: (this.state.mode === 'view') ? 'edit' : 'view'});
+    }
+
+    render() {
+        // pose={this.state.isBlob ? 'blob' : 'none'}
+        const {match} = this.props;
+
+        return <>
+            <div className="bg-white rounded item shadow-sm">
+                <Box2 pose={this.state.mode} className="d-flex">
+                    <div onClick={this.toggleMode} className="item-img rounded overflow-hidden">
+                        <Image fluid src={this.props.item.imgUrl}/>
+                    </div>
+                    <div onClick={this.toggleMode} className="item-content">
+                        <div className="h-100 d-flex justify-content-between ">
+                            <div className="d-flex flex-column pt-2 pb-2 pl-2 w-100">
+                                <div className="title">{this.props.item.title}</div>
+                                <div className="d-flex justify-content-between footer">
+                                    <small className="text-primary ">{this.props.item.tags}</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="item-toolbar d-flex justify-content-between flex-column p-2">
+                        <FontAwesomeIcon  className="cursor" icon={faEdit}/>
+                        <Box onClick={this.toggleLike}>
+                            <FontAwesomeIcon className={'cursor ' + (this.state.isLike ? 'text-danger' : '')}
+                                             icon={this.state.isLike ? faHeart : farHeart}/>
+                        </Box>
+                    </div>
+                </Box2>
 
 
-		this.toggleBlob = this.toggleBlob.bind(this);
+                <Editorn onMouseDown={this.toggleMode} className="item-content" pose={this.state.mode}>
+                    <div className="p-3"><FormExample/></div>
+                </Editorn>
 
+            </div>
 
-		this.renderToolbar = this.renderToolbar.bind(this);
-	}
+            {/*<div className="bg-white rounded item shadow-sm">*/}
+            {/*<div className="item-content p-3">*/}
+            {/*<div className="h-100 d-flex justify-content-between ">*/}
+            {/*<FormExample/>*/}
+            {/*</div>*/}
+            {/*</div>*/}
+            {/*</div>*/}
+        </>;
+    }
 
-	toggleBlob() {
-		this.setState({isBlob: !this.state.isBlob});
-	}
+    renderToolbar() { // fixme css
+        const view = <>
+            <FontAwesomeIcon onClick={this.toggleMode} className="cursor" icon={faEdit}/>
 
-	toggleLike() {
-		this.setState({isLike: !this.state.isLike});
-	}
+            <Box onClick={this.toggleLike}>
+                <FontAwesomeIcon
+                    className={'cursor ' + (this.state.isLike ? 'text-danger' : '')}
+                    icon={this.state.isLike ? faHeart : farHeart}/>
+            </Box>
 
-	toggleMode() {
-		this.setState({mode: (this.state.mode === 'view') ? 'edit' : 'view'});
-	}
+        </>;
 
-	render() {
-		const {match} = this.props;
-		return <BoxItem pose={this.state.isBlob ? 'blob' : 'none'}>
-			<div className="bg-white rounded item shadow-sm">
-				<div onMouseDown={this.toggleBlob} className="item-img rounded overflow-hidden">
-					{this.renderImage()}
-					{match}
-				</div>
-				<div className="item-content">
-					<div className="h-100 d-flex justify-content-between ">
-						<div onMouseDown={this.toggleBlob} className="d-flex flex-column pt-2 pb-2 pl-2 w-100">
-							<div className="title">{this.renderTitle()}</div>
-							<div className="d-flex justify-content-between footer">
-								{this.renderTags()}
-							</div>
-						</div>
-						<div className="item-toolbar d-flex justify-content-between flex-column p-2">
-							{this.renderToolbar()}
-						</div>
-					</div>
-				</div>
-			</div>
-		</BoxItem>;
-	}
+        const edit = <>
+            <FontAwesomeIcon onClick={this.toggleMode} className="cursor" icon={faTimesCircle}/>
+            <FontAwesomeIcon className="cursor" icon={faTrashAlt}/>
+            <FontAwesomeIcon className="cursor" icon={faCheckCircle}/>
+        </>;
 
-	renderImage() {
-		return (this.state.mode === 'view')
-			? <Image fluid src={this.props.item.imgUrl}/> : <input className="w-100" type={`text`}/>;
-	}
-
-	renderTitle() {
-		return (this.state.mode === 'view')
-			? this.props.item.title : <input className="w-100" type={`text`}/>;
-	}
-
-	renderTags() {
-		return (this.state.mode === 'view')
-			? <small className="text-primary ">{this.props.item.tags}</small> :
-			<input className="w-100" type={`text`}/>;
-	}
-
-	renderToolbar() { // fixme css
-		const view = <>
-			<FontAwesomeIcon onClick={this.toggleMode} className="cursor" icon={faEdit}/>
-
-			<Box onClick={this.toggleLike}>
-				<FontAwesomeIcon
-					className={'cursor ' + (this.state.isLike ? 'text-danger' : '')}
-					icon={this.state.isLike ? faHeart : farHeart}/>
-			</Box>
-
-		</>;
-
-		const edit = <>
-			<FontAwesomeIcon onClick={this.toggleMode} className="cursor" icon={faTimesCircle}/>
-			<FontAwesomeIcon className="cursor" icon={faTrashAlt}/>
-			<FontAwesomeIcon className="cursor" icon={faCheckCircle}/>
-		</>;
-
-		return this.state.mode === 'view' ? view : edit;
-	}
+        return this.state.mode === 'view' ? view : edit;
+    }
 }
 
 export default Item;
