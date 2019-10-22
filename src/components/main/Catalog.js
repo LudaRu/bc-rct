3a38933cbe02c3e3fb427d84c0dc8ec419d953e4
Ruslan M <manizin.r.s@effective-group.ru>
2019-10-22 11:05:07 +0300

    xxx

--- src/components/main/Catalog.js
+++ src/components/main/Catalog.js
@@ -3,12 +3,13 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Item from "./catalog/Item";
 import Masonry from 'react-masonry-css';
+import srcimg from './1547367999_1.jpg';
 
 const item = {
     id: 1,
     // imgUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Square_Lattice_Tiling.svg/220px-Square_Lattice_Tiling.svg.png',
     // imgUrl: 'https://klike.net/uploads/posts/2019-01/1547367999_1.jpg',
-    imgUrl: 'http://happymodern.ru/wp-content/uploads/2015/08/Vertikalnoe_ozelenenie_37.jpg',
+    imgUrl: srcimg,
     title: 'Автоклав бойлер формовочный компрессор',
     tags: '123 ',
     createTime: '12:44',
@@ -39,20 +40,23 @@ class Catalog extends Component {
                     breakpointCols={{default: 3, 992: 2, 768: 1,}}
                     className="my-masonry-grid"
                     columnClassName="my-masonry-grid_column">
-                    {[...Array(50).keys()].map((v, i) => {
+                    {[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,,1,1,1,1,1,11,
+                    ,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,,11,1,1,1,1,1,1,1,1,1,
+                    1,1,1,1,1,1,1,1,1,,1,1,1,1,11,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                    ].map((v, i) => {
                         return <Item item={item} index={i}/>;
                     })}
                 </Masonry>
 
-                <Row>
-                    {[...Array(50).keys()].map(v => {
-                        // return <Col className="" xs={12} md={6} lg={4}>
-                        //     <div className={`pb-3 pt-sm-3 pb-sm-3`}>
-                        //         <Item item={item}/>
-                        //     </div>
-                        // </Col>
-                    })}
-                </Row>
+                {/*<Row>*/}
+                    {/*{[...Array(50).keys()].map(v => {*/}
+                        {/*// return <Col className="" xs={12} md={6} lg={4}>*/}
+                        {/*//     <div className={`pb-3 pt-sm-3 pb-sm-3`}>*/}
+                        {/*//         <Item item={item}/>*/}
+                        {/*//     </div>*/}
+                        {/*// </Col>*/}
+                    {/*})}*/}
+                {/*</Row>*/}
             </Container>
         );
     }
