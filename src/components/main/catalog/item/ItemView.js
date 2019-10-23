96bf9253c30fd75bf52302f43c71d2bf69414153
sm <r>
2019-10-23 23:56:50 +0300

    x

--- src/components/main/catalog/item/ItemView.js
+++ src/components/main/catalog/item/ItemView.js
@@ -3,6 +3,7 @@ import Inputs from "../../../test/Inputs";
 import './style.css';
 import {CSSTransition, TransitionGroup} from "react-transition-group";
 import FormExample from "../../../test/FormExample";
+import {Button, ButtonGroup} from "react-bootstrap";
 
 class ItemView extends Component {
     constructor(props) {
@@ -18,26 +19,24 @@ class ItemView extends Component {
 
     render() {
         const {modeEdit} = this.state;
-        return (<div className="p-2">
-            <div onClick={() => {
-                this.setState({modeEdit: false});
-            }}>1
-            </div>
-            <div onClick={() => {
-                this.setState({modeEdit: true});
-            }}>2
-            </div>
+        return (<div className="">
             <TransitionGroup>
                 <CSSTransition
                     timeout={0}
                     classNames="page"
                     key={modeEdit}
                 >
-                    <div className="wrapper">
+                    <div className="wrapper p-2">
                         <BackgroundImage item={this.props.item} modeEdit={modeEdit}/>
                     </div>
                 </CSSTransition>
             </TransitionGroup>
+            <div className="bottom_btn">
+                <ButtonGroup size="sm" className="w-100">
+                    <Button onClick={() => {this.setState({modeEdit: true});}} >Редактировать</Button>
+                    <Button variant="secondary">Закрыть</Button>
+                </ButtonGroup>
+            </div>
         </div>);
     }
 }
